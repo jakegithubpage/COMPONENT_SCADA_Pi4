ba648c26adc0105ec1afb3ebf3eef67807cbe3ed
jakegithubpage <164205961+jakegithubpage@users.noreply.github.com>
2026-01-11 17:05:58 -0500

    Implement MQTT support for keypad control
    Added MQTT functionality to keypad application, enabling remote control and status updates. Integrated JSON parsing for command handling.

--- SCADA_COMPS/SCADA_KEYPAD/main.c
+++ SCADA_COMPS/SCADA_KEYPAD/main.c
@@ -10,17 +10,21 @@
 #include "esp_event.h"
 #include "esp_netif.h"
 #include "nvs_flash.h"
+#include "cJSON.h"
+#include "mqtt_client.h"
+#include <errno.h>
 
-/* =========================================================
- * GLOBAL CONFIG (must be before use)
- * ========================================================= */
 static const char *TAG = "KEYPAD";
 
 #define WIFI_SSID      "xxxxx"
 #define WIFI_PASS      "xxxxx"
 
 #define GATEWAY_IP     "x.x.x.x"  
-#define GATEWAY_PORT   9100          
+#define GATEWAY_PORT   9100    
+
+#define MQTT_URI       "mqtt://x.x.x.x:1885"
+#define MQTT_SUB_TOPIC "sensors/keypad/0/cmd"
+#define MQTT_PUB_TOPIC "sensors/keypad/0/status"
 
 #define DEVICE_ID      1
 
@@ -52,16 +56,15 @@ static const char *TAG = "KEYPAD";
 #define KEYPAD_DEBOUNCE_MS     30
 #define KEYPAD_SCAN_DELAY_US  5
 
-/* -------------------------
- * Passcode
- * ------------------------- */
+//passcode and check vars
 #define PASSCODE_LEN 4
 static const char PASSCODE[PASSCODE_LEN + 1] = "0000";
 static bool wifi_connected = false;
+static bool keypadEnabled = false;
+static bool changeCheck = true;
+static esp_mqtt_client_handle_t mqtt_client;
 
-/* =========================================================
- * TYPES
- * ========================================================= */
+//types
 typedef enum {
     KEY_NONE = 0,
     KEY_1 = 1, KEY_2, KEY_3, KEY_A,
@@ -100,9 +103,9 @@ static const char key_to_char[17] = {
     '*','0','#','D'
 };
 
-/* =========================================================
- * KEYPAD DRIVER
- * ========================================================= */
+
+
+
 static void keypad_init(void)
 {
     gpio_config_t io = {0};
@@ -135,6 +138,114 @@ static void keypad_init(void)
     /* Idle rows HIGH */
     for (int i = 0; i < KEYPAD_ROWS; i++) {
         gpio_set_level(row_pins[i], 1);
+    }
+}
+
+static void mqtt_event_handler(void *arg,
+                               esp_event_base_t base,
+                               int32_t id,
+                               void *data)
+{
+    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)data;
+
+    switch(event->event_id)
+    {
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI("MQTT", "Connected to broker");
+
+            esp_mqtt_client_subscribe(
+                mqtt_client,
+                MQTT_SUB_TOPIC,
+                1
+                );
+            break;
+
+        case MQTT_EVENT_DISCONNECTED:
+            ESP_LOGW("MQTT", "DISCONNECTED");
+            break;
+        
+        case MQTT_EVENT_DATA:
+             ESP_LOGI("MQTT",
+                     "RX topic=%.*s data=%.*s",
+                     event->topic_len,
+                     event->topic,
+                     event->data_len,
+                     event->data);
+            
+            if (strncmp(event->topic,
+                        MQTT_SUB_TOPIC,
+                        event->topic_len) == 0)
+            {
+
+                cJSON *root = cJSON_ParseWithLength(event->data, event->data_len);
+
+                if (root) 
+                {
+                    cJSON *enable = cJSON_GetObjectItem(root, "enable");
+
+                    if (cJSON_IsBool(enable))
+                    {
+                        keypadEnabled = cJSON_IsTrue(enable);
+                        ESP_LOGI(TAG, "KEYPAD %s (JSON)",
+                        keypadEnabled ? "ENABLED" : "DISABLED");
+                        cJSON_Delete(root);
+                        break;
+
+                    }
+                    cJSON_Delete(root);
+                }
+                if (strncmp(event->data, "ON", event->data_len) == 0)
+                {
+                    keypadEnabled = true;
+                    
+                }
+                else if (strncmp(event->data, "OFF", event->data_len) == 0)
+                {
+                    keypadEnabled = false;
+                }
+                else 
+                {
+                    ESP_LOGW(TAG, "Uknown payload -> Unable to identify");
+
+                }
+            }
+            break;
+
+        case MQTT_EVENT_ERROR:
+
+            esp_mqtt_error_type_t et = event->error_handle->error_type;
+            ESP_LOGE("MQTT", "MQTT_EVENT_ERROR type=%d", (int)et);
+            break;
+
+        default:
+            break;
+        }
+        
+    
+}
+
+static void mqtt_start(void)
+{
+    esp_mqtt_client_config_t cfg = {
+        .broker.address.uri = MQTT_URI,
+    };
+
+    mqtt_client = esp_mqtt_client_init(&cfg);
+    esp_mqtt_client_register_event(mqtt_client,
+                                   ESP_EVENT_ANY_ID,
+                                   mqtt_event_handler,
+                                   NULL);
+    esp_mqtt_client_start(mqtt_client);
+}
+
+
+void print_ip(void)
+{
+    esp_netif_ip_info_t ip;
+    esp_netif_t *netif = esp_netif_get_handle_from_ifkey("WIFI_STA_DEF");
+
+    if (netif && esp_netif_get_ip_info(netif, &ip) == ESP_OK) {
+        ESP_LOGI(TAG, "ESP32 IP: " IPSTR, IP2STR(&ip.ip));
     }
 }
 
@@ -264,54 +375,20 @@ static void wifi_event_handler(void* arg,
         
     }
 }
-
-static void wifi_init(void)
+static void send_keypad(void *pvParameters)
 {
-    esp_netif_init();
-    esp_event_loop_create_default();
-    esp_netif_create_default_wifi_sta();
 
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
-                               &wifi_event_handler, NULL);
-    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
-                               &wifi_event_handler, NULL);
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&cfg);
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = WIFI_SSID,
-            .password = WIFI_PASS,
-        },
-    };
-
-    esp_wifi_set_mode(WIFI_MODE_STA);
-    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
-    esp_wifi_start();
-}
-
-/* =========================================================
- * APP MAIN
- * ========================================================= */
-void app_main(void)
-{
-    int retries = 0;
-    ESP_LOGI(TAG, "system started");
-    ESP_ERROR_CHECK(nvs_flash_init()); 
-    keypad_init();
-    LED_init();
-    wifi_init();
-    while(!wifi_connected && (retries <= 50)) {
-        vTaskDelay(pdMS_TO_TICKS(200));
-        retries++;
-    }
     char entered[PASSCODE_LEN + 1] = {0};
     int entered_len = 0;
 
     while (1) {
-        keypad_key_t key = keypad_getkey();
 
+        if(keypadEnabled){
+        keypad_key_t key = keypad_getkey();
+        if(changeCheck == true){
+            ESP_LOGI(TAG, "Keypad enabled");
+            changeCheck = false;
+        }
         if (key != KEY_NONE) {
             char ch = (key < 17) ? key_to_char[key] : '?';
             ESP_LOGI(TAG, "Key pressed:%c", ch);
@@ -367,6 +444,72 @@ void app_main(void)
             }
         }
 
-        vTaskDelay(pdMS_TO_TICKS(10));
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
+    else{
+        if(changeCheck == false)
+        {
+            ESP_LOGI(TAG, "Keypad Disabled");
+            changeCheck = true;
+        }
+         vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}   
+}
+
+
+static void wifi_init(void)
+{
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
+                               &wifi_event_handler, NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
+                               &wifi_event_handler, NULL);
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        },
+    };
+
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    esp_wifi_start();
+}
+
+/* =========================================================
+ * APP MAIN
+ * ========================================================= */
+void app_main(void)
+{
+    int retries = 0;
+    ESP_LOGI(TAG, "system started");
+    ESP_ERROR_CHECK(nvs_flash_init()); 
+    keypad_init();
+    LED_init();     
+    print_ip();
+    wifi_init();
+   
+    
+    while(!wifi_connected && (retries <= 50)) {
+        vTaskDelay(pdMS_TO_TICKS(200));
+        retries++;
+    }
+    mqtt_start();
+
+    xTaskCreate(
+        send_keypad,
+        "send_keypad",
+        4096,
+        NULL,
+        5,
+        NULL
+    );
 }
