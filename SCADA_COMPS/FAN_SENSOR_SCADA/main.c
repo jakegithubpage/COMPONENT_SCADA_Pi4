0f8ebe04d8aa25c1b2597d2e740938ec2dba3a20
jakegithubpage <164205961+jakegithubpage@users.noreply.github.com>
2026-01-11 17:03:02 -0500

    Implement MQTT client for sensor control
    Added MQTT functionality to handle sensor commands and status updates.

--- SCADA_COMPS/FAN_SENSOR_SCADA/main.c
+++ SCADA_COMPS/FAN_SENSOR_SCADA/main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -11,7 +10,9 @@
 #include "esp_netif.h"
 #include "nvs_flash.h"
 #include "esp_log.h"
-
+#include "cJSON.h"
+#include "mqtt_client.h"
+#include <errno.h>
 #include "driver/gpio.h"
 
 #include "lwip/sockets.h"
@@ -28,9 +29,17 @@
 #define GATEWAY_PORT   9100           
 #define DEVICE_ID 2
 
-static const char *TAG = "ESP32";
-static bool wifi_connected = false;
+#define MQTT_URI       "mqtt://x.x.x.x:1885"
+#define MQTT_SUB_TOPIC "sensors/mosense/0/cmd"
+#define MQTT_PUB_TOPIC "sensors/mosense/0/status"
 
+static const char *TAG = "MO_SENSE";
+static bool wifi_connected = false;
+static bool mosenseEnabled = false;
+
+static bool changeCheck = true;
+
+static esp_mqtt_client_handle_t mqtt_client;
 
 static void wifi_event_handler(void* arg,
                                esp_event_base_t event_base,
@@ -54,6 +63,114 @@ static void wifi_event_handler(void* arg,
     }
 }
 
+
+static void mqtt_event_handler(void *arg,
+                               esp_event_base_t base,
+                               int32_t id,
+                               void *data)
+{
+    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)data;
+
+    switch(event->event_id)
+    {
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI("MQTT", "Connected to broker");
+
+            esp_mqtt_client_subscribe(
+                mqtt_client,
+                MQTT_SUB_TOPIC,
+                1
+                );
+            break;
+
+        case MQTT_EVENT_DISCONNECTED:
+            ESP_LOGW("MQTT", "DISCONNECTED");
+            break;
+        
+        case MQTT_EVENT_DATA:
+             ESP_LOGI("MQTT",
+                     "RX topic=%.*s data=%.*s",
+                     event->topic_len,
+                     event->topic,
+                     event->data_len,
+                     event->data);
+            
+            if (strncmp(event->topic,
+                        MQTT_SUB_TOPIC,
+                        event->topic_len) == 0)
+            {
+
+                cJSON *root = cJSON_ParseWithLength(event->data, event->data_len);
+
+                if (root) 
+                {
+                    cJSON *enable = cJSON_GetObjectItem(root, "enable");
+
+                    if (cJSON_IsBool(enable))
+                    {
+                        mosenseEnabled = cJSON_IsTrue(enable);
+                        ESP_LOGI(TAG, "MOSENSE %s (JSON)",
+                        mosenseEnabled ? "ENABLED" : "DISABLED");
+                        cJSON_Delete(root);
+                        break;
+
+                    }
+                    cJSON_Delete(root);
+                }
+                if (strncmp(event->data, "ON", event->data_len) == 0)
+                {
+                    mosenseEnabled = true;
+                    
+                }
+                else if (strncmp(event->data, "OFF", event->data_len) == 0)
+                {
+                    mosenseEnabled = false;
+                }
+                else 
+                {
+                    ESP_LOGW(TAG, "Uknown payload -> Unable to identify");
+
+                }
+            }
+            break;
+
+        case MQTT_EVENT_ERROR:
+
+            esp_mqtt_error_type_t et = event->error_handle->error_type;
+            ESP_LOGE("MQTT", "MQTT_EVENT_ERROR type=%d", (int)et);
+            break;
+
+        default:
+            break;
+        }
+        
+    
+}
+
+
+static void mqtt_start(void)
+{
+    esp_mqtt_client_config_t cfg = {
+        .broker.address.uri = MQTT_URI,
+    };
+
+    mqtt_client = esp_mqtt_client_init(&cfg);
+    esp_mqtt_client_register_event(mqtt_client,
+                                   ESP_EVENT_ANY_ID,
+                                   mqtt_event_handler,
+                                   NULL);
+    esp_mqtt_client_start(mqtt_client);
+}
+
+void print_ip(void)
+{
+    esp_netif_ip_info_t ip;
+    esp_netif_t *netif = esp_netif_get_handle_from_ifkey("WIFI_STA_DEF");
+
+    if (netif && esp_netif_get_ip_info(netif, &ip) == ESP_OK) {
+        ESP_LOGI(TAG, "ESP32 IP: " IPSTR, IP2STR(&ip.ip));
+    }
+}
 
 static void wifi_init(void)
 {
@@ -164,11 +281,22 @@ static void send_sensor_state(int gpio, int state)
 static void sensor_task(void *arg)
 {
     //delay to calibrate
-    vTaskDelay(pdMS_TO_TICKS(15000));
+    vTaskDelay(pdMS_TO_TICKS(50));
     int last_state = gpio_get_level(SENPIN);
 
-    while(1)
+
+while(1)
+{
+    if(mosenseEnabled)
     {
+
+    if(changeCheck)
+    {
+        ESP_LOGI(TAG,"Motion Sensor Enabled");
+        changeCheck = false;
+    }
+    
+    
         int state = gpio_get_level(SENPIN);
 
         if (state != last_state)
@@ -191,8 +319,19 @@ static void sensor_task(void *arg)
             
         }
         vTaskDelay(pdMS_TO_TICKS(40));
+    
+    }
+    else {
+        if(!changeCheck)
+        {
+            ESP_LOGI(TAG,"Motion Sensor Disabled");
+            changeCheck = true;
+        }
     }
 }
+}
+
+
 
 
 void app_main(void)
@@ -201,8 +340,9 @@ void app_main(void)
     ESP_LOGE(TAG, "Bootin up...");
     nvs_flash_init();
     sensor_gpio_init();
-    fan_init();
+    
     LED_init();
+    print_ip();
 
     wifi_init();
 
@@ -211,6 +351,7 @@ void app_main(void)
         vTaskDelay(pdMS_TO_TICKS(200));
 
     }
+    mqtt_start();
 
     xTaskCreate(
         sensor_task,
